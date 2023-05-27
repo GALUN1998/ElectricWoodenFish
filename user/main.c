bd21ba4dc7d722903a555d1f4ec75b78d94b8d3a
galun <galun1998@163.com>
2023-05-28 01:06:02 +0800

    init project struct

---
+++ user/main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+// 声明区域
+void init();
+
+int main()
+{
+    printf("Hello EletricWoodenFish\n");
+    // 初始化
+    init();
+    return 0;
+}
+
+void init() 
+{
+    printf("Init EletricWoodenFish\n");
+}
